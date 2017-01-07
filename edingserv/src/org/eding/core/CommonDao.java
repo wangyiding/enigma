3378998628410c6430a4a51288b85ad1a8f18b5b
eding <cise-canser@163.com>
2017-01-07 16:42:21 +0800

    使用prepareStatement

--- edingserv/src/org/eding/core/CommonDao.java
+++ edingserv/src/org/eding/core/CommonDao.java
@@ -69,7 +69,7 @@ public class CommonDao<T> {
 	public List<T> findByExample(Object instance) {
 		try {
 			List results = getSession()
-					.createCriteria("org.eding.pro.po.CUserTbl")
+					.createCriteria(instance.getClass().getName())
 					.add(create(instance)).list();
 			return results;
 		} catch (RuntimeException re) {
@@ -101,11 +101,61 @@ public class CommonDao<T> {
 		start=(page-1)*pageSize;//起始位置
 		String sqlRight=" limit "+start+","+pageSize+"  ";
 		PagedResult pagedResult=new PagedResult();
-		sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS");
+		sql=sql.toLowerCase();
+		sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS ");
 		List resultMapList=selectMapBySql(sql+sqlRight);
-		System.out.println(sql+sqlRight);
 		pagedResult.setResultList(resultMapList);
-		List CountList=selectMapBySql("select FOUND_ROWS() as recCount");
+		List CountList=selectMapBySql(" select FOUND_ROWS() as recCount ");
+		pagedResult.setReccount((Long)((Map)(CountList.get(0))).get("reccount"));
+		pagedResult.setMaxPages((pagedResult.getReccount()+pageSize-1)/pageSize);
+		return pagedResult;
+	}
+	/**
+	 * @param sql   预期执行的sql
+	 * @param page  查询的页数，
+	 * @param pageSize 每页的大小
+	 * @return  分页结果集  map承装
+	 * @throws HibernateException
+	 * @throws SQLException
+	 * @author 王一丁
+	 * 使用preparesql查询map结果
+	 */
+	public PagedResult selectPagedMapResultByprepareSql(String sql,Object parameter,int page,int pageSize) throws HibernateException, SQLException{
+		int start=0;
+		page=(page<=0?1:page);//修正页码
+		start=(page-1)*pageSize;//起始位置
+		String sqlRight=" limit "+start+","+pageSize+"  ";
+		PagedResult pagedResult=new PagedResult();
+		sql=sql.toLowerCase();
+		sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS ");
+		List resultMapList=selectMapByPreparedSql(sql+sqlRight,parameter);
+		pagedResult.setResultList(resultMapList);
+		List CountList=selectMapBySql(" select FOUND_ROWS() as recCount ");
+		pagedResult.setReccount((Long)((Map)(CountList.get(0))).get("reccount"));
+		pagedResult.setMaxPages((pagedResult.getReccount()+pageSize-1)/pageSize);
+		return pagedResult;
+	}
+	/**
+	 * @param sql   预期执行的sql
+	 * @param page  查询的页数，
+	 * @param pageSize 每页的大小
+	 * @return  分页结果集  map承装
+	 * @throws HibernateException
+	 * @throws SQLException
+	 * @author 王一丁
+	 * 使用preparesql查询map结果
+	 */
+	public PagedResult selectPagedMapResultByprepareSql(String sql,Object[] parameter,int page,int pageSize) throws HibernateException, SQLException{
+		int start=0;
+		page=(page<=0?1:page);//修正页码
+		start=(page-1)*pageSize;//起始位置
+		String sqlRight=" limit "+start+","+pageSize+"  ";
+		PagedResult pagedResult=new PagedResult();
+		sql=sql.toLowerCase();
+		sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS ");
+		List resultMapList=selectMapByPreparedSql(sql+sqlRight,parameter);
+		pagedResult.setResultList(resultMapList);
+		List CountList=selectMapBySql(" select FOUND_ROWS() as recCount ");
 		pagedResult.setReccount((Long)((Map)(CountList.get(0))).get("reccount"));
 		pagedResult.setMaxPages((pagedResult.getReccount()+pageSize-1)/pageSize);
 		return pagedResult;
@@ -129,7 +179,6 @@ public class CommonDao<T> {
 		PagedResult pagedResult=new PagedResult();
 		sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS");
 		List resultMapList= selectObjectBySql(sql+sqlRight,clazz);
-		System.out.println(sql+sqlRight);
 		pagedResult.setResultList(resultMapList);
 		List CountList=selectMapBySql("select FOUND_ROWS() as recCount");
 		pagedResult.setReccount((Long)((Map)(CountList.get(0))).get("reccount"));
@@ -137,6 +186,57 @@ public class CommonDao<T> {
 		return pagedResult;
 	}
 	
+public PagedResult<T> selectPagedObjectResultByPreparedSql(String sql,Object param,int page,int pageSize,Class clazz) throws HibernateException, SQLException{
+		
+		int start=0;
+		page=(page<=0?1:page);//修正页码
+		start=(page-1)*pageSize;//起始位置
+		String sqlRight=" limit "+start+","+pageSize+"  ";
+		PagedResult pagedResult=new PagedResult();
+		sql=sql.toLowerCase();
+		sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS");
+		List resultMapList= selectObjectByPreparedSql(sql+sqlRight,param,clazz);
+		pagedResult.setResultList(resultMapList);
+		List CountList=selectMapBySql("select FOUND_ROWS() as recCount");
+		pagedResult.setReccount((Long)((Map)(CountList.get(0))).get("reccount"));
+		pagedResult.setMaxPages((pagedResult.getReccount()+pageSize-1)/pageSize);
+		return pagedResult;
+	}
+
+
+public PagedResult<T> selectPagedObjectResultByPreparedSql(String sql,Object[] param,int page,int pageSize,Class clazz) throws HibernateException, SQLException{
+	
+	int start=0;
+	page=(page<=0?1:page);//修正页码
+	start=(page-1)*pageSize;//起始位置
+	String sqlRight=" limit "+start+","+pageSize+"  ";
+	PagedResult pagedResult=new PagedResult();
+	sql=sql.toLowerCase();
+	sql=sql.replaceFirst("select ", "select SQL_CALC_FOUND_ROWS");
+	List resultMapList= selectObjectByPreparedSql(sql+sqlRight,param,clazz);
+	pagedResult.setResultList(resultMapList);
+	List CountList=selectMapBySql("select FOUND_ROWS() as recCount");
+	pagedResult.setReccount((Long)((Map)(CountList.get(0))).get("reccount"));
+	pagedResult.setMaxPages((pagedResult.getReccount()+pageSize-1)/pageSize);
+	return pagedResult;
+}
+
+	
+	
+	public  List<Map> selectMapByPreparedSql(String sql,Object param) throws HibernateException, SQLException{
+		return (List<Map>) queryRunner.query(getSession().connection(), sql,param, new MapListHandler());
+	}
+	
+	public  List<Map> selectMapByPreparedSql(String sql,Object param[]) throws HibernateException, SQLException{
+		return (List<Map>) queryRunner.query(getSession().connection(), sql,param, new MapListHandler());
+	}
+	
+	public List selectObjectByPreparedSql(String sql,Object param,Class clasz) throws SQLException{
+		return (List) queryRunner.query(getSession().connection(), sql,param,new BeanListHandler(clasz));
+	}
+	public List selectObjectByPreparedSql(String sql,Object param[],Class clasz) throws SQLException{
+		return (List) queryRunner.query(getSession().connection(), sql,param,new BeanListHandler(clasz));
+	}
 	
 	
 }	
